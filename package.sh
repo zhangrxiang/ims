9697ed22e844016ea2fba7b2f88e43f45cde4bea
zing <599490911@qq.com>
2020-09-02 11:28:24 +0800

    添加定时删除缓存文件功能

---
+++ package.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+NAME=simple-ims
+
+echo "=============run=====start=========="
+
+starttime=$(date '+%Y年%m月%d日 %H时%M分%S秒')
+starttime2=$(date +'%Y-%m-%d %H:%M:%S')
+echo "开始时间：$starttime"
+
+echo "build....start..."
+if [ -f ./${NAME}.exe ]; then
+  ./${NAME}.exe build
+  else
+    go build
+  ./${NAME}.exe init
+fi
+
+go build
+echo "----------------------------------------------"
+./${NAME}.exe info
+echo "----------------------------------------------"
+
+echo "build....over..."
+
+version=$(./${NAME}.exe version | awk '{print $2}')
+
+if [ ! -d "./production" ]; then
+  mkdir ./production
+fi
+
+tar -czvf ./production/${NAME}."${version}".tar.gz ${NAME}.exe \
+service.ini.example \
+uninstall.bat \
+install.bat \
+www
+
+endtime=$(date '+%Y年%m月%d日 %H时%M分%S秒')
+endtime2=$(date +'%Y-%m-%d %H:%M:%S')
+echo "结束时间：$endtime"
+start_seconds=$(date --date="$starttime2" +%s)
+end_seconds=$(date --date="$endtime2" +%s)
+echo "本次运行时间： "$((end_seconds - start_seconds))"s"
+
+echo "=============run=====end=========="
